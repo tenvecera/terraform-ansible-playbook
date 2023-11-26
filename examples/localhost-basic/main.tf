958fb5ed1a0eb3e8b27f2573658a448d36aec691
Josef Vecera <tenvecera@gmail.com>
2023-11-26 17:33:03 +0100

    update examples

--- examples/localhost-basic/main.tf
+++ examples/localhost-basic/main.tf
@@ -1,9 +1,15 @@
 module "ansible-playbook" {
-  source  = "tenvecera/ansible/playbook"
-  version = "1.0.0"
+  source  = "tenvecera/playbook/ansible"
+  version = "2.0.0"
 
   playbook_file = "${path.module}/main.yml"
   inventory = {
     ips = ["127.0.0.1"]
   }
+
+  collection_requirements_file = "${path.module}/collections/requirements.yml"
+  collections_path             = "${path.module}/collections/ansible_collections"
+  galaxy_collection_install_args = [
+    "--force"
+  ]
 }
