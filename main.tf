1d3265c80e49c9963e179d3a008a313d6d09d2be
Josef Vecera <tenvecera@gmail.com>
2023-11-26 17:31:33 +0100

    ansible galaxy rework

--- main.tf
+++ main.tf
@@ -1,5 +1,32 @@
 locals {
   inventory = lookup(var.inventory, "ips", null) != null ? "${join(",", var.inventory.ips)}," : "${path.module}/inventory.yml"
+  collection_sha1 = sha1(join(
+    "", concat(
+      [
+        var.collections_path,
+        var.collection_requirements_file == "" ? "" : filesha1(var.collections_path)
+      ],
+      var.galaxy_collection_install_args
+    )
+  ))
+  role_sha1 = sha1(join(
+    "", concat(
+      [
+        var.roles_path,
+        var.role_requirements_file == "" ? "" : filesha1(var.role_requirements_file)
+      ],
+      var.galaxy_role_install_args
+    )
+  ))
+  playbook_sha1 = sha1(join(
+    "", concat(
+      [
+        local.inventory,
+        filesha1(var.playbook_file)
+      ],
+      var.playbook_extra_args
+    )
+  ))
 }
 
 resource "local_file" "inventory" {
@@ -11,16 +38,46 @@ resource "local_file" "inventory" {
 
 resource "null_resource" "run-playbook" {
   triggers = {
-    run = var.force_run ? "${timestamp()}" : "run_once"
+    run                  = var.force_run ? "${timestamp()}" : "run_once"
+    change_in_collection = local.collection_sha1
+    change_in_role       = local.role_sha1
+    change_in_playbook   = local.playbook_sha1
   }
 
   provisioner "local-exec" {
     command = <<EOT
       export ANSIBLE_FORCE_COLOR=1
       export PYTHONBUFFERED=1
-      [ -f "${var.role_requirements_file}" ] && ansible-galaxy role install -r ${var.role_requirements_file} || echo "[skip] ansible-galaxy role install." 1>&2
-      [ -f "${var.collection_requirements_file}" ] && ansible-galaxy collection install -r ${var.collection_requirements_file} || echo "[skip] anislbe-galaxy collection install." 1>&2
-      ansible-playbook -i ${local.inventory} ${var.ansible_extra_args} ${var.playbook_file}
+
+      if [ -f "$COLLECTION_REQUIREMENTS_FILE" ]; then
+        ansible-galaxy collection install \
+        ${join(" ", var.galaxy_collection_install_args)} \
+        --collections-path="$ANSIBLE_COLLECTIONS_PATH" \
+        --requirements-file="$COLLECTION_REQUIREMENTS_FILE"
+      else
+        echo "[skip] ansible-galaxy collection install"
+      fi
+
+      if [ -f "$ROLE_REQUIREMENTS_FILE" ]; then
+        ansible-galaxy role install \
+        ${join(" ", var.galaxy_role_install_args)} \
+        --roles-path="$ANSIBLE_ROLES_PATH" \
+        --role-file="$ROLE_REQUIREMENTS_FILE"
+      else
+        echo "[skip] ansible-galaxy role install"
+      fi
+
+      ansible-playbook \
+        ${join(" ", var.playbook_extra_args)} \
+        -i ${local.inventory} \
+        ${var.playbook_file}
     EOT
+
+    environment = {
+      ANSIBLE_COLLECTIONS_PATH     = var.collections_path == "" ? pathexpand("~/.ansible/collections") : var.collections_path
+      COLLECTION_REQUIREMENTS_FILE = var.collection_requirements_file
+      ANSIBLE_ROLES_PATH           = var.roles_path == "" ? pathexpand("~/.ansible/roles") : var.roles_path
+      ROLE_REQUIREMENTS_FILE       = var.role_requirements_file
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -9,10 +9,34 @@ variable "role_requirements_file" {
   description = "Path to file with role requirements for playbook"
 }
 
+variable "roles_path" {
+  type        = string
+  default     = ""
+  description = "The path to the directory containing your roles."
+}
+
+variable "galaxy_role_install_args" {
+  type        = list
+  default     = []
+  description = "Passed arguments to ansible-galaxy role install command; doc: https://docs.ansible.com/ansible/latest/cli/ansible-galaxy.html#role-install. Note: role_file and roles_path will be ignored in favor of variables role_requirements_file and roles_path."
+}
+
 variable "collection_requirements_file" {
   type        = string
   default     = ""
   description = "Path to file with collection requirements for playbook"
+}
+
+variable "collections_path" {
+  type        = string
+  default     = ""
+  description = "The path to the directory containing your collections."
+}
+
+variable "galaxy_collection_install_args" {
+  type        = list
+  default     = []
+  description = "Passed arguments to ansible-galaxy collection install command; doc: https://docs.ansible.com/ansible/latest/cli/ansible-galaxy.html#collection-install. Note: requirements_file and collections_path will be ignored in favor of variables collection_requirements_file and collections_path."
 }
 
 variable "inventory" {
@@ -31,9 +55,9 @@ variable "inventory" {
   }
 }
 
-variable "ansible_extra_args" {
-  type        = string
-  default     = ""
+variable "playbook_extra_args" {
+  type        = list
+  default     = []
   description = "Extra arguments which will be passed to ansible-playbook command"
 }
 
