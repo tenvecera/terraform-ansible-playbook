5e67baa157443dbcf545fd612abe4efbd7e4a4f5
Josef Vecera <tenvecera@gmail.com>
2023-11-26 17:40:05 +0100

    local example refactoring

--- examples/localhost-basic/main.tf
+++ examples/localhost-basic/main.tf
@@ -1,14 +1,18 @@
+locals {
+  ansible_path = "${path.module}/ansible"
+}
+
 module "ansible-playbook" {
   source  = "tenvecera/playbook/ansible"
   version = "2.0.0"
 
-  playbook_file = "${path.module}/main.yml"
+  playbook_file = "${local.ansible_path}/main.yml"
   inventory = {
     ips = ["127.0.0.1"]
   }
 
-  collection_requirements_file = "${path.module}/collections/requirements.yml"
-  collections_path             = "${path.module}/collections/ansible_collections"
+  collection_requirements_file = "${local.ansible_path}/collections/requirements.yml"
+  collections_path             = "${local.ansible_path}/collections/ansible_collections"
   galaxy_collection_install_args = [
     "--force"
   ]
